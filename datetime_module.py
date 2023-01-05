8ea1656e47f692ca70821ed985d5c323e39fec79
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-01-05 15:01:43 +0530

    Add files via upload

---
+++ datetime_module.py
@@ -0,0 +1,24 @@
+import datetime
+print(datetime.datetime.today())
+# present_date=datetime.datetime.now()
+# print(present_date)
+# formatted_date=present_date.strftime("%A/%B/%y")
+# print(formatted_date)
+# print(type(formatted_date))
+# print(datetime.date.today()
+
+
+
+# import datetime
+# now=datetime.datetime.now()
+# other=datetime.datetime(2020,7,20,22,56)
+# print(now-other)
+
+
+
+# future_date=present_date+timedelta(days=1095)
+# print(type(future_date))
+# print("future_date_3years:",str(future_date))
+# print(future_date-present_date)
+
+
